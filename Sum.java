9b2adf2d22200e0454dc4536307c05fa05c1f739
Amrit2003 <140589272+Amrit2003@users.noreply.github.com>
2025-10-15 16:40:29 +0530

    Checking conflict

--- Sum.java
+++ Sum.java
@@ -2,7 +2,9 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("The Sum is " + a*b);
+        System.out.println("We are learning conflicts");
+        System.out.println("The Sum is " + (a*b));
+        System.out.println("Tommorow We have a diwali party");
     }
 
 }
