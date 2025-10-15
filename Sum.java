8b1edf84bd8a30807f0352c4d0591b5e3a5f256b
sanctionedpranav <pranav9728@gmail.com>
2025-10-15 16:36:24 +0530

    change in sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        System.out.println("The Sum is " + a ^ b);
+        System.out.println("The Sum will be shown below");
+        System.out.println("The Sum is " + (a ^ b));
     }
 
 }
