28bc49b4bb066adaeff1c5e98299abe9256b7779
BrijeshKasaudhan <brijeshkasaudhan8715@gmail.com>
2025-10-15 16:58:40 +0530

    brijesh chenge the sum.java file

--- Sum.java
+++ Sum.java
@@ -2,7 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 30;
-        System.out.println("The Sum is = " + a + b);
+        System.out.println("The Sum  of a & b is = " + a + b);
+
     }
 
 }
