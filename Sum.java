04a9e6a115af08ce3722cabd8643c47d8ff3a5c6
RitikaSrivastava2204 <ritika.ingzb@gmail.com>
2025-10-15 16:32:03 +0530

    I have made one more change to sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        System.out.println("Sum is :" + (a + b));
+        System.out.println("Hello, I am Ritika from Git Collaboration project.");
         System.out.println("I am making a changes in Sum.java file");
     }
 
