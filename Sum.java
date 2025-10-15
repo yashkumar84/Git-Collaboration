dcdb0b43286880de9d88600b4d57a28c8b0a7758
RitikaSrivastava2204 <ritika.ingzb@gmail.com>
2025-10-15 16:28:43 +0530

    made a change in sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        
+        System.out.println("I am making a changes in Sum.java file");
     }
 
 }
+ 
